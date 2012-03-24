8eeb0f84a33213bfd671a30b02c0b11341963e95
palms831nathaniel@gmail.com <palms831nathaniel@gmail.com@3b72c405-c151-4882-03fa-fc77871a7164>
2012-03-24 02:42:26 +0000

    Added methods for direction control and checking the snake's status (dead or alive) to snake.h.

--- branches/npalm479/bit-biter/snake.h
+++ branches/npalm479/bit-biter/snake.h
@@ -18,6 +18,8 @@ class Snake : public QObject
 {
     Q_OBJECT
 public:
+	enum Direction {UP, DOWN, LEFT, RIGHT};
+
     explicit Snake(QObject *parent = 0);
 
 	//--------------------------------------------------------------
@@ -33,9 +35,44 @@ public:
 
     QList<QPoint> *getBodySegments();
 
+	//--------------------------------------------------------------
+	// bool checkIsDead()
+	// Purpose: Retrieves value indicating whether or not the snake
+	// has died. Death occurs on collision with any obstacle.
+	// Limitations: none
+	// Assumptions: none
+	// Return: true if the snake is dead, false if it is still alive
+	//--------------------------------------------------------------
+
+	bool checkIsDead();
+
+	//--------------------------------------------------------------
+	// void changeDirection(Snake::Direction newDirection)
+	// Purpose: Sets the snake's direction of movement.
+	// Limitations: all attempts to change the direction to its opposite
+	// (ie. changing LEFT to  RIGHT) are ignored
+	// Assumptions: none
+	// Return: nothing
+	//--------------------------------------------------------------
+
+	void changeDirection(Snake::Direction newDir);
+
+	//--------------------------------------------------------------
+	// void move()
+	// Purpose: Attempts to move snake one unit forward. If food is in
+	// the snakes path it is eaten. If a collision occurs the snake dies.
+	// Limitations: does nothing if the snake has already died
+	// Assumptions: none
+	// Return: nothing
+	//--------------------------------------------------------------
+
+	void move();
+
 private:
     QList<QPoint> *bodySegments;
     bool isDead;
+	Direction direction;
+
 
 signals:
 
