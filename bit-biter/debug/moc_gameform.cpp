2291cfc37c023cbb16fafe1884a464ec39e71082
palms831nathaniel@gmail.com <palms831nathaniel@gmail.com@3b72c405-c151-4882-03fa-fc77871a7164>
2012-03-24 02:42:26 +0000

    Added methods for direction control and checking the snake's status (dead or alive) to snake.h.

--- bit-biter/debug/moc_gameform.cpp
+++ bit-biter/debug/moc_gameform.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'gameform.h'
 **
-** Created: Mon Mar 12 21:21:08 2012
+** Created: Fri Mar 16 15:40:56 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.3)
 **
 ** WARNING! All changes made in this file will be lost!
