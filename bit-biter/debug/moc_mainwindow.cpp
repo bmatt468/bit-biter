51672874bb7d6f19a776fc53af972a21e6f21e2e
bnmtthews <bnmtthews@3b72c405-c151-4882-03fa-fc77871a7164>
2012-03-25 18:01:42 +0000

    Merged trunk and bmatt468 branch

--- bit-biter/debug/moc_mainwindow.cpp
+++ bit-biter/debug/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Fri Mar 23 23:02:09 2012
+** Created: Sun Mar 25 14:00:18 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.3)
 **
 ** WARNING! All changes made in this file will be lost!
