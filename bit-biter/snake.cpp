23abab8bb336b6cdf1ac47780891fe7d6a2ad485
palms831nathaniel@gmail.com <palms831nathaniel@gmail.com@3b72c405-c151-4882-03fa-fc77871a7164>
2012-03-25 01:07:40 +0000

    Added initial direction parameter to Snake constructor.

--- bit-biter/snake.cpp
+++ bit-biter/snake.cpp
@@ -1,6 +1,6 @@
 #include "snake.h"
 
-Snake::Snake(QObject *parent, QPoint headLoc, int length) :
+Snake::Snake(QObject *parent, QPoint headLoc, int length, Direction dir) :
     QObject(parent)
 {
 }
