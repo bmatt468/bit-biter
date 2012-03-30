1156ef6e0181b65c5bae63e7421b005327415422
palms831nathaniel@gmail.com <palms831nathaniel@gmail.com@3b72c405-c151-4882-03fa-fc77871a7164>
2012-03-30 16:50:08 +0000

    Merged changes from trunk.

---
+++ branches/npalm479/bit-biter/options.cpp
@@ -0,0 +1,21 @@
+#include "options.h"
+#include "ui_options.h"
+
+Options::Options(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Options)
+{
+    ui->setupUi(this);
+}
+
+Options::~Options()
+{
+    delete ui;
+}
+
+void Options::on_body_clicked()
+{
+    QColorDialog *qc = new QColorDialog();
+    qc->show();
+
+}
