d46eb76ca5ec7e1622f66d8d304418c2020c4919
palms831nathaniel@gmail.com <palms831nathaniel@gmail.com@3b72c405-c151-4882-03fa-fc77871a7164>
2012-03-30 12:11:18 +0000

    Merged changes from trunk.

--- branches/npalm479/bit-biter/gameform.cpp
+++ branches/npalm479/bit-biter/gameform.cpp
@@ -23,41 +23,64 @@ void GameForm::closeEvent(QCloseEvent *event)
 
 void GameForm::refreshBoard()
 {
-    QList<QPushButton*> oldSegments = this->findChildren<QPushButton *>();
-    for (int i = 0; i < oldSegments.count(); ++i)
+    if (!g->checkIsGameOver())
     {
-        oldSegments.value(i)->deleteLater();
+        QList<QPushButton*> oldSegments = this->findChildren<QPushButton *>();
+        for (int i = 0; i < oldSegments.count(); ++i)
+        {
+            oldSegments.value(i)->deleteLater();
+        }
+
+        s = g->getSnake();
+        snakesegs = s->getBodySegments();
+
+        QSize qz(20,20);
+        for (int i = 0; i < snakesegs->count(); ++i)
+        {
+            QPushButton *snakeSeg = new QPushButton(this);
+            snakeSeg->setObjectName("snake");
+            QRect qr(snakesegs->value(i) *= 20, qz);
+            snakeSeg->setGeometry(qr);
+            snakeSeg->setStyleSheet("background-color: rgb(0,0,255);");
+            snakeSeg->show();
+        }
+
+        QRect foodLoc(g->getFood() * 20, qz);
+        QPushButton *food = new QPushButton(this);
+        food->setGeometry(foodLoc);
+        food->setStyleSheet("background-color: rgb(0,255,0);");
+        food->show();
     }
 
-    s = g->getSnake();
-    snakesegs = s->getBodySegments();
-
-    QSize qz(20,20);
-    for (int i = 0; i < snakesegs->count(); ++i)
+    else
     {
-        QPushButton *test = new QPushButton(this);
-        QRect qr(snakesegs->value(i) *= 20, qz);
-        test->setGeometry(qr);
-        test->show();
-    }
+        QList<QPushButton*> deadSnake = this->findChildren<QPushButton *>("snake");
+        for (int i = 0; i < deadSnake.count(); ++i)
+        {
+            deadSnake.value(i)->hide();
+            deadSnake.value(i)->setStyleSheet("background-color: rgb(255,0,0)");
+            deadSnake.value(i)->show();
+        }
 
-    QRect foodLoc(g->getFood() * 20, qz);
-    QPushButton *food = new QPushButton(this);
-    food->setGeometry(foodLoc);
-    food->show();
+        l->setStyleSheet("border-image: url(:/fonts/fonts/gameover.png);");
+        l->setGeometry(10,10,375,50);
+        l->show();
+    }
 
 }
 
 void GameForm::focusOutEvent(QFocusEvent *)
 {    
     l->setStyleSheet("border-image: url(:/fonts/fonts/paused.png);");
-    l->setGeometry(10,10,500,75);
+    l->setGeometry(10,10,300,50);
     l->show();
+    g->pause();
 }
 
 void GameForm::focusInEvent(QFocusEvent *)
 {
     l->hide();
+    g->start();
 }
 
 void GameForm::keyPressEvent(QKeyEvent *event)
